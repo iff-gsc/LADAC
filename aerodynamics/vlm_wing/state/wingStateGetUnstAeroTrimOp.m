b8c9ff4d88baef70bd18784cc710de36042733a9
Yannic Beyer <y.beyer@tu-bs.de>
2022-06-30 18:48:59 +0200

    VLM wing: improve unsteady trimming accuracy

--- aerodynamics/vlm_wing/state/wingStateGetUnstAeroTrimOp.m
+++ aerodynamics/vlm_wing/state/wingStateGetUnstAeroTrimOp.m
@@ -20,11 +20,10 @@ switch wing_config.airfoil_method
         % get coefficients of analytic functions for different Mach numbers
         fcl = airfoilAnalytic0515Ma( wing_airfoil.analytic.wcl, wing_state.aero.circulation.Ma );
         % get points on lift curve
-        [c_L_alpha,alpha_0] = airfoilAnalytic0515ClAlphaMax( fcl, wing_state.aero.circulation.Ma );
+        [c_L_alpha_deg,alpha_0] = airfoilAnalytic0515ClAlphaMax( fcl, wing_state.aero.circulation.Ma );
+        c_L_alpha = rad2deg(c_L_alpha_deg);
         % effective angle of attack for an equivalent uncambered airfoil
         alpha_inf_0 = wing_state.aero.circulation.alpha_eff - deg2rad(alpha_0);
-        [c_N_alpha_max,alpha_0] = airfoilAnalytic0515ClAlphaMax( fcl, wing_state.aero.circulation.Ma );
-        c_N_alpha_max = rad2deg(c_N_alpha_max);
         alpha_0_rad = deg2rad(alpha_0);
     case 'simple'
         % effective angle of attack for an equivalent uncambered airfoil
@@ -66,22 +65,34 @@ switch wing_config.airfoil_method
         
         [ c_L_c, ~, c_L_nc, ~, ~, ~ ] = ...
             unstAirfoilAeroFast( abs_V_i, wing_state.aero.circulation.Ma, ...
-            wing_state.geometry.ctrl_pt.c, rad2deg(c_L_alpha), x_ac, ...
+            wing_state.geometry.ctrl_pt.c, c_L_alpha, x_ac, ...
             x, alpha_inf_0, q );
 
         C_N_p = ( c_L_c + c_L_nc );
         
-        alpha_f = C_N_p ./ c_N_alpha_max;
-        c_L_st_f = airfoilAnalytic0515AlCl( fcl, [ rad2deg(alpha_f+alpha_0_rad); wing_state.aero.circulation.Ma ] );
+        X1 = C_N_p;
+        
+        while true
+        
+            alpha_f = X1 ./ c_L_alpha;
+            c_L_st_f = airfoilAnalytic0515AlCl( fcl, [ rad2deg(alpha_f+alpha_0_rad); wing_state.aero.circulation.Ma ] );
 
-        f_s = airfoilDynStallFst( c_L_st_f, deg2rad(c_N_alpha_max), rad2deg(alpha_f) );
+            f_s = airfoilDynStallFst( c_L_st_f, deg2rad(c_L_alpha), rad2deg(alpha_f) );
 
-        c_v = zeros( size(f_s) );
+            c_v = zeros( size(f_s) );
 
-        u = [ C_N_p; 1-f_s; c_v ];
-
-        % assume that aerodynamics is steady state
-        X = u;
+            u = [ C_N_p; 1-f_s; c_v ];
+            
+            % assume that aerodynamics is steady state
+            X = u;
+            
+            if sum(abs(X1-X(1,:))) < 1e-8
+                break;
+            else
+                X1 = X(1,:);
+            end
+            
+        end
     case 'simple'
         X = zeros( size( wing_state.aero.unsteady.X ) );
     otherwise %%%
