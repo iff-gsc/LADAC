f709afd77e1bd78feac2e1ce69cbc719d645948f
Yannic Beyer <y.beyer@tu-bs.de>
2022-08-02 11:32:31 +0200

    Structure: add wing cut loads function
    Until now, there was only one function for calculating
    the bending moment. Now a new function (based on the bending
    moment function) has been created, which can calculate all
    types of cutting loads.
    The bending moment function has been retained. However, it
    now calls the new function.

--- equations_of_motion/structure/structureGetBendingMomentTrafoAt.m
+++ equations_of_motion/structure/structureGetBendingMomentTrafoAt.m
@@ -35,39 +35,9 @@ function [md2bm, span_load_station] = structureGetBendingMomentTrafoAt( structur
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
-Phi_Lg = zeros( length(eta), size( structure.K,1 ) );
-span_load_station = zeros(1,length(eta));
-
-% node indices from wing tip to wing root
-idx_left    = structureGetNodeIdxFromWingTipToRoot( structure, 'left' );
-idx_right   = structureGetNodeIdxFromWingTipToRoot( structure, 'right' );
-
-% for each eta one row in the transformation matrix is computed
-for i = 1:length(eta)
-    % We check from wing root when we reach the specified eta.
-    % All remaining node indices will be considered to compute the bending
-    % moment. However, we have to use unique since idx_side could contain
-    % duplicate numbers.
-    if eta(i) < 0
-        idx_side = idx_left;
-    else
-        idx_side = idx_right;
-    end
-    wing_nodes_pos_tip_to_root = structure.xyz(:,idx_side);
-    is_outside_flip = abs(flip(wing_nodes_pos_tip_to_root(2,:))) > abs(eta(i) * wing_nodes_pos_tip_to_root(2,1));
-    idx_outside = idx_side(flip(is_outside_flip));
-    idx_use = unique(idx_outside);
-    
-    % consider y lever arm and z force to compute bending moment
-    Phi_Lg( i, (idx_use-1)*6+3 ) = -abs( structure.xyz(2,idx_use) - abs(eta(i)) * wing_nodes_pos_tip_to_root(2,1) );
-%     Phi_Lg( i, (idx_use-1)*6+2 ) = -abs( structure.xyz(2,idx_use) - abs(eta(i)) * wing_nodes_pos_tip_to_root(3,1) );
-%     Phi_Lg( i, (idx_use-1)*6+4 ) = 1;
-
-    span_load_station(i) = eta(i) * wing_nodes_pos_tip_to_root(2,1);
-end
-
-
 % matrix product of [1], eq. (11)
-md2bm = Phi_Lg * structure.K * structure_red.modal.T;
-
+[md2bm,span_load_station] = structureGetCutLoadTrafoAt( ...
+                                structure_red, structure, eta, ...
+                                'Mx', 'reverse_right' );
+                                    
 end
