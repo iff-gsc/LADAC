00717b27f0829bc2314e18bb977fda94c32eb1d0
Yannic Beyer <y.beyer@tu-bs.de>
2022-09-12 19:09:42 +0200

    Control allocation: clean up and rename from "control_allocation" to "ca"
    "ca" is short and unique.

--- control/control_allocation/caIndiWlsBlockInit.m
+++ control/control_allocation/caIndiWlsBlockInit.m
@@ -1,6 +1,7 @@
-function [] = controlAllocationIndiWlsHlcBlockInit(is_Delta_W_v_checked,...
+function [] = caIndiWlsBlockInit(is_Delta_u_d_checked,...
+    is_Delta_gamma_checked,is_Delta_W_v_checked,...
     is_W_checked,is_iter_checked)
-%controlAllocationIndiWlsHlcBlockInit init block "INDI high level wls control allocation for copters"
+%caIndiWlsBlockInit init block "INDI wls control allocation"
 %   If block check box is enabled, create new block inport or outport.
 %   If block check box is disabled, delete new block inport or outport.
 
@@ -11,7 +12,9 @@ function [] = controlAllocationIndiWlsHlcBlockInit(is_Delta_W_v_checked,...
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
-blockInitToggleInport( 'Delta_diag_W_v', is_Delta_W_v_checked );
+blockInitToggleInport( 'Delta u_d', is_Delta_u_d_checked );
+blockInitToggleInport( 'Delta gamma', is_Delta_gamma_checked );
+blockInitToggleInport( 'Delta diag W_v', is_Delta_W_v_checked );
 
 blockInitToggleOutport( 'W', is_W_checked );
 blockInitToggleOutport( 'iter', is_iter_checked );
--- control/control_allocation/caIndiWlsHlcBlockInit.m
+++ control/control_allocation/caIndiWlsHlcBlockInit.m
@@ -1,7 +1,6 @@
-function [] = controlAllocationIndiWlsBlockInit(is_Delta_u_d_checked,...
-    is_Delta_gamma_checked,is_Delta_W_v_checked,...
+function [] = caIndiWlsHlcBlockInit(is_Delta_W_v_checked,...
     is_W_checked,is_iter_checked)
-%controlAllocationIndiWlsBlockInit init block "INDI wls control allocation"
+%caIndiWlsHlcBlockInit init block "INDI high level wls control allocation for copters"
 %   If block check box is enabled, create new block inport or outport.
 %   If block check box is disabled, delete new block inport or outport.
 
@@ -12,9 +11,7 @@ function [] = controlAllocationIndiWlsBlockInit(is_Delta_u_d_checked,...
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
-blockInitToggleInport( 'Delta u_d', is_Delta_u_d_checked );
-blockInitToggleInport( 'Delta gamma', is_Delta_gamma_checked );
-blockInitToggleInport( 'Delta diag W_v', is_Delta_W_v_checked );
+blockInitToggleInport( 'Delta_diag_W_v', is_Delta_W_v_checked );
 
 blockInitToggleOutport( 'W', is_W_checked );
 blockInitToggleOutport( 'iter', is_iter_checked );
