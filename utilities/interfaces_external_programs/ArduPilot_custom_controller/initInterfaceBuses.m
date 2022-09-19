d77191ff08c0ef0dceae807b494413724c45d3aa
Yannic Beyer <y.beyer@tu-bs.de>
2022-09-19 12:33:43 +0200

    ArduPilot interface: Add measured values to ArduPilot interface
    More signals were added to the measureBus (see initInterFaceBus).
    Hence, the Simulink block "receive measureBus from ArduPilot SITL
    had to be adjusted. initInterfaceBuses was converted to a function
    because the struct variables are not needed in base workspace.
    The sample time variable is not created anymore and has to be
    set by the user.
    The user can now specify (function input of initInterfaceBuses)
    whether the interface variables are single or double precision.
    Double precision caused cryptic variable names for the cmd and
    measure bus after code generation.

--- utilities/interfaces_external_programs/ArduPilot_custom_controller/initInterfaceBuses.m
+++ utilities/interfaces_external_programs/ArduPilot_custom_controller/initInterfaceBuses.m
@@ -1,4 +1,5 @@
-%% measured values from ahrs object
+function [] = initInterfaceBuses( varargin )
+
 
 % Disclamer:
 %   SPDX-License-Identifier: GPL-2.0-only
@@ -7,8 +8,23 @@
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
+
+if isempty(varargin)
+    data_type = 'single';
+else
+    data_type = varargin{1};
+end
+
+%% measured values from ahrs object
+
 % angular velocity of FRD frame relative to the earth represented in FRD
-% frame, in rad/s
+% frame, in rad/s; This unfiltered (and noisy) signal is obtained on a
+% custom way and should be preferred over omega_Kb and then processed by a
+% custom low-pass filter.
+measure.Omega_Kb_raw = zeros(3,1);
+% angular velocity of FRD frame relative to the earth represented in FRD
+% frame as used internally in ArduPilot (Omega_Kb_raw should be perferred
+% because omega_Kb is still noisy due to unknown reasons), in rad/s
 measure.omega_Kb    = zeros(3,1);
 % Euler angles of FRD frame relative to NED frame, in 1
 measure.EulerAngles = zeros(3,1);
@@ -52,10 +68,14 @@ cmd.num_waypoints  = uint16(0);
 % RC PWM input
 cmd.RC_pwm         = zeros(16,1);
 
-%% SampleTime for ArduPilot (s)
-
-cntrl.sample_time   = 1/400;
-
 %% create bus object
+
+if strcmp(data_type,'single')
+    measure = structDouble2Single(measure);
+    cmd = structDouble2Single(cmd);
+end
+
 struct2bus( measure, 'measureBus' );
 struct2bus( cmd, 'cmdBus' );
+
+end
