f84ed6b48e67255f88646cb3864e4b9a71043f28
Yannic Beyer <y.beyer@tu-bs.de>
2022-10-18 19:20:22 +0200

    ArduPilot interface: Avoid data copying for logging
    Previously, the output of the "log muxer" Simulink
    block contained a buffer for the maximum of to be
    logged signals plus parameters to indicate the labels
    and signal names. Unfortunately in the generated C++
    code all these outputs were copied each time the
    step() function was called even if not needed.
    Moreover, the logging could only be initialized after
    calling the step() function in the generated C++ code
    a first time which does not look clean.
    Now the output of the "log muxer" Simulink block only
    contains the signals to be logged. The parameters to
    indicate the labels and signal names are now defined
    by a "Simulink.Parameter" named "log_config" in the
    Matlab base workspace. The "log_config" parameter is
    created automatically by the mask initialization of the
    "log muxer" block calling function ardupilotInitLogMuxer.
    The "log_config" parameter is configured such that it
    appears as initialized const parameter in the generated
    C++ code (in MatlabController.cpp).

--- utilities/interfaces_external_programs/ArduPilot_custom_controller/ardupilotCreateLogBus.m
+++ utilities/interfaces_external_programs/ArduPilot_custom_controller/ardupilotCreateLogBus.m
@@ -1,11 +1,11 @@
-function [] = ardupilotCreateLogBus(varargin)
-% ardupilotCreateLogBus create Simulink bus object "logBus" for ArduPilot logs
+function [log_config] = ardupilotCreateLogBus()
+% ardupilotCreateLogBus create Simulink bus object "logConfigBus" for ArduPilot logs
 %   ArduPilot logs can be configured using the Simulink block "log muxer".
 %   The output of that block is an array of nonvirtual buses. For these
 %   nonvirtual buses the data type must be provided. That is why this
-%   function exists and why we need the variable "logBus" in the base
+%   function exists and why we need the variable "logConfigBus" in the base
 %   workspace.
-%   Fortunately, the "logBus" will automatically be created during the
+%   Fortunately, the "logConfigBus" will automatically be created during the
 %   initialization of the Simulink block "log muxer". For more information
 %   have a look at the "log muxer" block.
 % 
@@ -16,23 +16,18 @@ function [] = ardupilotCreateLogBus(varargin)
 %   -
 % 
 % Outputs:
-%   none (but a Simulink bus object named "logBus" will be assigned to the
-%   base workspace)
+%   log_config              struct corresponding to the Simulink bus object
+%                           "logConfigBus" that will be assigned to the
+%                           base workspace
 % 
 % See also:
-%   ardupilotInitLogMuxer, ardupilotInitLogMuxerCore
+%   ardupilotInitLogMuxer, ardupilotCreateInputBuses
 
 % Disclamer:
 %   SPDX-License-Identifier: GPL-2.0-only
 % 
 %   Copyright (C) 2022 Yannic Beyer
 % *************************************************************************
-
-if isempty(varargin)
-    data_type = 'single';
-else
-    data_type = varargin{1};
-end
 
 num_char_per_signal         = 3;
 num_char_per_batch          = 4;
@@ -41,16 +36,10 @@ num_signals_per_batch_max   = 14;
 max_label_char_length   = num_signals_per_batch_max * num_char_per_signal;
 label_char_length       = max_label_char_length;
 
-log.signals        = zeros(num_signals_per_batch_max,1);
-log.num_signals    = uint8(0);
-log.signal_names   = uint8(zeros(label_char_length,1));
-log.batch_name     = uint8(zeros(num_char_per_batch,1));
+log_config.num_signals 	= uint8(0);
+log_config.signal_names	= uint8(zeros(label_char_length,1));
+log_config.batch_name	= uint8(zeros(num_char_per_batch,1));
 
-
-if strcmp(data_type,'single')
-    log = structDouble2Single(log);
-end
-
-struct2bus( log, 'logBus' );
+struct2bus( log_config, 'logConfigBus' );
 
 end
--- utilities/interfaces_external_programs/ArduPilot_custom_controller/ardupilotInitLogMuxer.m
+++ utilities/interfaces_external_programs/ArduPilot_custom_controller/ardupilotInitLogMuxer.m
@@ -1,4 +1,4 @@
-function [] = ardupilotInitLogMuxer( block_path, batch_name, num_signals,varargin )
+function [] = ardupilotInitLogMuxer( block_path, batch_name, num_signals )
 % ardupilotInitLogMuxer initialize ArduPilot Log Muxer Simulink block
 %   This function is called from the initialization of the Simulink block
 %   log muxer.
@@ -9,8 +9,7 @@ function [] = ardupilotInitLogMuxer( block_path, batch_name, num_signals,varargi
 %   in the base workspace.
 % 
 % Syntax:
-%   ardupilotInitLogMuxer( gcb, batch_name, num_signals )
-%   ardupilotInitLogMuxer( gcb, batch_name, num_signals, data_type )
+%   ardupilotInitLogMuxer( block_path, batch_name, num_signals )
 % 
 % Inputs:
 %   Take a look at the mask of the "log muxer" Simulink block.
@@ -20,8 +19,7 @@ function [] = ardupilotInitLogMuxer( block_path, batch_name, num_signals,varargi
 %   -
 % 
 % See also:
-%   ardupilotCreateLogBus, ardupilotInitLogMuxerCore,
-%   ardupilotCreateLogSignalNames
+%   ardupilotCreateLogBus, ardupilotCreateLogSignalNames
 
 % Disclamer:
 %   SPDX-License-Identifier: GPL-2.0-only
@@ -29,37 +27,169 @@ function [] = ardupilotInitLogMuxer( block_path, batch_name, num_signals,varargi
 %   Copyright (C) 2022 Yannic Beyer
 % *************************************************************************
 
-num_signals_per_batch_max = 14;
-    
-ardupilotCreateLogBus(varargin{:});
+persistent memory;
 
-num_batches_max = length(batch_name);
+num_batches_max = 5;
 
-default_log_names = ardupilotDefaultLogNames(num_signals_per_batch_max);
+inport_path = find_system(block_path,'LookUnderMasks','on','FollowLinks','on','SearchDepth',1,'BlockType','Inport');
+num_inports = length(inport_path);
 
-ardupilotResetLogMuxer(block_path,batch_name);
-for i = 1:num_batches_max
-    for j = 1:num_signals_per_batch_max
-        inport_name = [batch_name{i},'.',default_log_names{j}];
-        if j <= num_signals(i)
-            is_enabled = true;
-        else
-            is_enabled = false;
-        end
-        blockInitToggleInport( inport_name, is_enabled );
-        if is_enabled
-            inport_path = find_system(block_path,'LookUnderMasks','on','FollowLinks','on','SearchDepth',1,'Name',inport_name);
-            if i == 1
-                port_number = j;
-            else
-                port_number =  sum(num_signals(1:i-1)) + j;
-            end
-            set_param(inport_path{1},'Port',num2str(port_number));
-        end
+if ~isempty(memory)
+    if num_inports ~= sum(memory.num_signals)
+        memory = [];
     end
 end
 
+if isempty(memory)
+    batch_name_init = cell(1,num_batches_max);
+    num_signals_init = zeros(1,num_batches_max);
+    idx_batch = 1;
+    for i = 1:num_inports
+        [~,b]=fileparts(inport_path{i});
+        if i == 1
+            batch_name_init{1} = b;
+            num_signals_init(1) = 1;
+        else
+            if ~strcmp(b,batch_name_init{idx_batch})
+                idx_batch = idx_batch + 1;
+            end
+            batch_name_init{idx_batch} = b;
+            num_signals_init(idx_batch) = num_signals_init(idx_batch) + 1;
+        end
+    end
+    memory.batch_name = batch_name_init;
+    memory.num_signals = num_signals_init;
+end
+
+log = ardupilotCreateLogBus();
+
+num_signals_cum = cumsum(num_signals);
+
+mux_block_cell = find_system(block_path,'LookUnderMasks','on','FollowLinks','on','SearchDepth',1,'BlockType','Mux');
+
+line_handles = get_param(mux_block_cell{1},'LineHandles');
+input_line_handles = line_handles.Inport;
+
+% delete all lines to avoid if, else, ...
+deleteLines(input_line_handles);
+
+% port names: update batch name
+inport_path = updateBatchNames(inport_path,batch_name,memory);
+
+% create all ports
+inport_path = addRemovePorts(block_path,inport_path,batch_name,num_signals,memory);
+
+% set mux
+set_param(mux_block_cell{1},'Inputs',num2str(num_signals_cum(end)));
+
+% place inports next to mux inputs and connect them
+positionConnectInports(mux_block_cell{1},inport_path)
+
 [~,signal_names] = ardupilotCreateLogSignalNames( block_path, num_signals );
+
+setInportNames(num_signals,signal_names,batch_name);
+
+idx_start = cumsum([1,num_signals]);
+idx_end = cumsum(num_signals);
+log_config = repmat(log,1,num_batches_max);
+for i = 1:num_batches_max
+    log_config(i).num_signals(:) = uint8(num_signals(i));
+    if num_signals(i)>0
+        signal_names_i = uint8(signal_names(:,idx_start(i):idx_end(i)));
+        log_config(i).signal_names(1:numel(signal_names_i)) = signal_names_i;
+    end
+    batch_name_i = uint8(batch_name{i});
+    log_config(i).batch_name(1:length(batch_name_i)) = batch_name_i;
+end
+
+% avoid data copying in generated code
+log_config_param = Simulink.Parameter(log_config);
+log_config_param.CoderInfo.StorageClass = 'Custom';
+log_config_param.CoderInfo.CustomStorageClass = 'Const';
+
+assignin('base','log_config',log_config_param);
+
+memory.batch_name = batch_name;
+memory.num_signals = num_signals;
+
+end
+
+function [] = deleteLines(input_line_handles)
+for i = 1:length(input_line_handles)
+    if input_line_handles(i) ~= -1
+        delete_line(input_line_handles(i));
+    end
+end
+end
+
+function port_number = portNumber(num_signals_cum_0,i,j)
+port_number = num_signals_cum_0(i) + j;
+end
+
+function [inport_path] = updateBatchNames(inport_path,batch_name,memory)
+num_batches_max = 5;
+num_signals_cum = cumsum(memory.num_signals);
+num_signals_cum_0 = [0,num_signals_cum];
+for i = 1:num_batches_max
+    for j = 1:memory.num_signals(i)
+        pn = portNumber(num_signals_cum_0,i,j);
+        port_name = get_param(inport_path{pn},'Name');
+        port_name_new = strrep(port_name,memory.batch_name{i},batch_name{i});
+        set_param(inport_path{pn},'Name',port_name_new);
+        inport_path(pn) = strrep(inport_path{pn},memory.batch_name(i),batch_name(i));
+    end
+end
+end
+
+function [inport_path] = addRemovePorts(block_path,inport_path,batch_name,num_signals,memory)
+num_batches_max = 5;
+num_signals_cum = cumsum(num_signals);
+num_signals_cum_0 = [0,num_signals_cum];
+diff_num_signals = num_signals - memory.num_signals;
+for i = 1:num_batches_max
+    if diff_num_signals(i) > 0
+        for j = memory.num_signals(i)+1:num_signals(i)
+            pn = portNumber(num_signals_cum_0,i,j);
+            new_block_path = [block_path,'/',batch_name{i},'.','s',num2str(j)];
+            add_block('simulink/Sources/In1',new_block_path);
+            set_param(new_block_path,'Port',num2str(pn));
+            inport_path(pn+1:end+1) = inport_path(pn:end);
+            inport_path{pn}=new_block_path;
+        end
+    elseif diff_num_signals(i) < 0
+        for j = memory.num_signals(i):-1:num_signals(i)+1
+            pn = num_signals_cum_0(i) + j;
+            delete_block(inport_path{pn});
+            inport_path(pn) = [];
+        end
+    end
+end
+end
+
+function [] = positionConnectInports(mux_path,inport_path)
+Delta_pos_y = -200;
+all_ports = get_param(mux_path, 'PortConnectivity');
+input_pos = {all_ports(1:end-1).Position};
+num_signals = length(input_pos);
+inport_pos = get_param(inport_path{1},'position');
+inport_width = inport_pos(3)-inport_pos(1);
+inport_height = inport_pos(4)-inport_pos(2);
+block_path = fileparts(mux_path);
+for i = 1:num_signals
+    pos_left = input_pos{i}(1)+Delta_pos_y;
+    pos_top = input_pos{i}(2)-inport_height/2;
+    pos_right = input_pos{i}(1)+inport_width+Delta_pos_y;
+    pos_down = input_pos{i}(2)+inport_height/2;
+    set_param( inport_path{i}, 'position', [ pos_left, pos_top, pos_right, pos_down ] );
+    point_1_y = input_pos{i}(1)+Delta_pos_y+inport_width;
+    point_1_x = input_pos{i}(2);
+    point_2_y = input_pos{i}(1);
+    point_2_x = input_pos{i}(2);
+    add_line( block_path, [ point_1_y, point_1_x; point_2_y, point_2_x ] );
+end
+end
+
+function [] = setInportNames(num_signals,signal_names,batch_name)
 num_signals_total = sum(num_signals);
 num_signals_cumsum = cumsum([0,num_signals]);
 for i = 1:num_signals_total
@@ -67,50 +197,5 @@ for i = 1:num_signals_total
     inport_name = [char(batch_name(batch_idx)'),'.',char(signal_names(:,i)')];
     inport_name(inport_name==1)=[];
     blockInitRenameInport( i, inport_name );
-end
-
-end
-
-
-function [] = ardupilotResetLogMuxer( block_path, batch_name )
-% set default names for all source blocks
-
-num_batches = length(batch_name);
-
-subsys_block_names = repmat({'log muxer core'},num_batches,1);
-for ii = 1:num_batches
-    subsys_block_names{ii}(end+1) = num2str(ii);
-end
-
-for ii = 1:num_batches
-    subsys_block_cell = find_system(block_path,'LookUnderMasks','on','FollowLinks','on','SearchDepth',1,'Name',subsys_block_names{ii});
-
-    if isempty(subsys_block_cell)
-        error('ardupilotResetLogMuxer: Block is not named correctly.')
-    else
-        subsys_block_path = subsys_block_cell{1};
-    end
-
-    all_ports = get_param(subsys_block_path, 'PortConnectivity');
-
-    src_block_handles = [all_ports(1:end-1).SrcBlock];
-    
-    num_signals_per_batch_max = length(all_ports)-1;
-
-    default_log_names_ = ardupilotDefaultLogNames(num_signals_per_batch_max);
-
-    for jj = 1:length(src_block_handles)
-        src_block_name = [batch_name{ii},'.',default_log_names_{jj}];
-        set_param(src_block_handles(jj),'Name',src_block_name);
-    end
-end
-
-end
-
-
-function default_log_names = ardupilotDefaultLogNames(last_signal_number)
-default_log_names = {};
-for i = 1:last_signal_number
-    default_log_names{i} = ['s',num2str(i)];
 end
 end
