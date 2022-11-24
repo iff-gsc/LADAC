a91322e010aabef071d55d0c5ab3f2cbcd45998b
Yannic Beyer <y.beyer@tu-bs.de>
2022-11-24 18:04:20 +0100

    Battery: Add unit test
    Ref. #29
    Resolve #12

---
+++ propulsion/battery/batteryUnitTest.m
@@ -0,0 +1,21 @@
+% batteryUnitTest test function
+%
+% Example call:
+%   rt = table(runtests('batteryUnitTest'))
+
+% Disclamer:
+%   SPDX-License-Identifier: GPL-2.0-only
+% *************************************************************************
+
+% Call all subfunction tests in this file
+function tests = batteryUnitTest
+    tests = functiontests(localfunctions);
+end
+
+
+function batteryExampleScriptTest(testCase)
+
+run('battery_example');
+close all;
+
+end
