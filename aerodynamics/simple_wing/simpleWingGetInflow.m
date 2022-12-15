iff-gsc/LADAC

commit b28975d749b6d642b1179730706dd302f3764c04

Make more use of powerFast, sqrtReal, acosReal, ...

aerodynamics/simple_wing/simpleWingGetInflow.m
@@ -72,10 +72,8 @@ V_Ab_P      = [ velocityFromRot( V_Ab, Omega_Ab_v(:,1), xyz_wing_b ...
             + wing.xyz_cntrl_pt_wing(:,1) ), ...
             velocityFromRot( V_Ab, Omega_Ab_v(:,2), xyz_wing_b ...
             + wing.xyz_cntrl_pt_wing(:,2) ) ] + V_Ab_prop;
-argSqrt     = max( 0, V_Ab_P(1,:).^2 + V_Ab_P(2,:).^2 + ...
-                V_Ab_P(3,:).^2 );
-V_A_P       = real( sqrt( argSqrt ) );
-q_P         = rho/2 * V_A_P.^2;
+V_A_P       = sqrtReal( vecnorm(V_Ab_P,2,1) );
+q_P         = rho/2 * powerFast(V_A_P,2);
 
 % airspeed vector in wing frame for each wing side
 V_Aw_P = [ M_wb(:,:,1)*V_Ab_P(:,1), M_wb(:,:,2)*V_Ab_P(:,2) ];

aerodynamics/airfoil/dynamic_stall/airfoilDynStallFst.m
@@ -24,13 +24,13 @@ function f_st = airfoilDynStallFst( c_L_st, c_L_alpha, Delta_alpha ) %#codegen
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
-% avoid division by zero: inices
-idx = abs(Delta_alpha) <= 0.5;
+abs_Delta_alpha = abs(Delta_alpha);
 
-f_st = min( 1, powerFast( 2*(sqrtReal( c_L_st ./ ( c_L_alpha.*Delta_alpha.*cos(pi/2/90*Delta_alpha) ) ) ) - 1, 2 ) );
-f_st(abs(Delta_alpha) > 70) = 0;
+f_st = powerFast( 2*(sqrtReal( c_L_st ./ ( c_L_alpha.*Delta_alpha.*cos(pi/2/90*Delta_alpha) ) ) ) - 1, 2 );
 
+f_st(f_st>1) = 1;
+f_st(abs_Delta_alpha > 70) = 0;
 % avoid division by zero: set f_st to 1
-f_st(idx) = 1;
+f_st(abs_Delta_alpha < 0.5) = 1;
 
 end

aerodynamics/vlm_wing/interim_results/wingSetInterimResults.m
@@ -10,7 +10,7 @@ function interim_results = wingSetInterimResults( wing )
 
 v_inf = repmat([-1;0;0],1,wing.n_panel);
 spanwise_vector = wingGetDimLessSpanwiseLengthVector(wing.geometry.vortex);
-interim_results.sweep = pi/2 - acos( abs( dot( -spanwise_vector, -v_inf, 1 ) ) ./ ( vecnorm(-v_inf,2,1) .* vecnorm(spanwise_vector,2,1) ) );
+interim_results.sweep = pi/2 - acosReal( abs( dot( -spanwise_vector, -v_inf, 1 ) ) ./ ( vecnorm(-v_inf,2,1) .* vecnorm(spanwise_vector,2,1) ) );
 
 spanwise_vector_yz = spanwise_vector;
 spanwise_vector_yz(1,:) = 0;

control/NDI/multicopter/ndiCopterAcc2quatRed.m
@@ -32,7 +32,7 @@ function q_red = ndiCopterAcc2quatRed( a_g, g )
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
-a_g_xy = sqrt( a_g(1)^2 + a_g(2)^2 );
+a_g_xy = sqrtReal( powerFast(a_g(1),2) + powerFast(a_g(2),2) );
 
 % [1], eq. (50)
 phi_red = atan2( a_g_xy, a_g(3) + g );

flight_parameters/aeroAnglesMod.m
@@ -42,11 +42,9 @@ function [ alpha_M, beta_M ] = aeroAnglesMod( V_Ab ) %#codegen
 
 % compute the argument of the asin function
 argAlpha = V_Ab(3,:) ./ vecnorm( V_Ab, 2, 1 );
-% maximum absolute argument of the asin function (avoid complex numbers)
-a = 1;
-argAlpha = max( min( argAlpha, a), -a);
+
 % compute the modified angle of attack and avoid complex numbers
-alpha_M = real( asin( argAlpha ) );
+alpha_M = asinReal( argAlpha );
 
 % compute the modified sideslip angle
 beta_M = atan2( V_Ab(2,:), V_Ab(1,:) );

flight_parameters/flightPathBankAngle.m
@@ -36,7 +36,7 @@
 function mu_K = flightPathBankAngle( gamma, Phi, Theta, beta_K ) %#codegen
 
 % compute the flight-path bank angle according to [2, page 9]
-mu_K = asin( (sin(gamma)*sin(beta_K)+sin(Phi)*cos(Theta)) / ...
+mu_K = asinReal( (sin(gamma)*sin(beta_K)+sin(Phi)*cos(Theta)) / ...
     (cos(beta_K)*cos(gamma)) );
 
 end

utilities/axes_transformation/dcm2Euler.m
@@ -28,7 +28,7 @@ function EulerAngles  = dcm2Euler( M_bg ) %#codegen
 
 % compute the Euler angles according to [1, page 12]
 Phi = atan2( M_bg(2,3), M_bg(3,3) );
-Theta = -asin( M_bg(1,3) );
+Theta = -asinReal( M_bg(1,3) );
 Psi = atan2( M_bg(1,2), M_bg(1,1) );
 
 % build the vector of Euler angles

utilities/axes_transformation/dcm2Lean.m
@@ -30,13 +30,13 @@ function [phi,delta] = dcm2Lean( M_bg )
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
 % *************************************************************************
 
-phi = acos( max( -1, min( 1, M_bg(3,3) ) ) );
+phi = acosReal(M_bg(3,3));
 
 if M_bg(3,2) >= 0
     sign_m32 = -1;
 else
     sign_m32 = 1;
 end
-delta = atan2( sign_m32*sqrt( max( 0, sin(phi)^2 - M_bg(3,1)^2 ) ), -M_bg(3,1) );
+delta = atan2( sign_m32*sqrtReal( sin(phi)^2 - M_bg(3,1)^2 ), -M_bg(3,1) );
 
 end

commit d8df48d12e77bc5c7723496dea068976776a779b

Simple wing: Bug fix: The airspeed was significantly too small
The bug was introduced in Commit b28975d7.
Instead of the airspeed the square root of the airspeed
was used. This caused significantly too small aerodynamic
forces.

aerodynamics/simple_wing/simpleWingGetInflow.m
@@ -72,7 +72,7 @@ V_Ab_P      = [ velocityFromRot( V_Ab, Omega_Ab_v(:,1), xyz_wing_b ...
             + wing.xyz_cntrl_pt_wing(:,1) ), ...
             velocityFromRot( V_Ab, Omega_Ab_v(:,2), xyz_wing_b ...
             + wing.xyz_cntrl_pt_wing(:,2) ) ] + V_Ab_prop;
-V_A_P       = sqrtReal( vecnorm(V_Ab_P,2,1) );
+V_A_P       = vecnorm(V_Ab_P,2,1);
 q_P         = rho/2 * powerFast(V_A_P,2);
 
 % airspeed vector in wing frame for each wing side