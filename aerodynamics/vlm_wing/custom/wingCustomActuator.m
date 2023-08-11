9c450298ba9837057a7988c51573b76e9bebd257
Yannic Beyer <y.beyer@tu-bs.de>
2023-08-11 15:03:44 +0200

    VLM wing: Avoid custom actuator dimension error

--- aerodynamics/vlm_wing/custom/wingCustomActuator.m
+++ aerodynamics/vlm_wing/custom/wingCustomActuator.m
@@ -1,6 +1,6 @@
 function [c_L,c_m,c_D,z2_dt] = wingCustomActuator(wing)
-c_L = zeros(1,wing.n_panel);
-c_m = zeros(1,wing.n_panel);
-c_D = zeros(1,wing.n_panel);
+c_L = zeros(size(wing.state.aero.unsteady.c_L_act2));
+c_m = zeros(size(wing.state.aero.unsteady.c_L_act2));
+c_D = zeros(size(wing.state.aero.unsteady.c_L_act2));
 z2_dt = zeros(size(wing.state.aero.unsteady.z2_dt));
 end
