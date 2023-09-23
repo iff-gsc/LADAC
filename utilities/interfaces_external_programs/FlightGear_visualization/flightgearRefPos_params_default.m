48d6f337815c288c19ecb8884c4b751efa95fbc1
Yannic Beyer <y.beyer@tu-bs.de>
2023-09-23 18:30:30 +0200

    FlightGear interface: Add default reference position

---
+++ utilities/interfaces_external_programs/FlightGear_visualization/flightgearRefPos_params_default.m
@@ -0,0 +1,8 @@
+% ** parameters for reference position ** 
+
+% initial latitude, in deg
+param.lat = 37.6117;
+% initial longitude, in deg
+param.lon = -122.37822;
+% initial altitude, in m
+param.alt = 150;
