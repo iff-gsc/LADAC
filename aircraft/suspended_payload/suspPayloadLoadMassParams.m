007ed1f755bc23f108fa131828d629e0bced1063
Jonas Withelm <j.withelm@tu-braunschweig.de>
2024-08-11 17:05:59 +0200

    Add suspended payload library

---
+++ aircraft/suspended_payload/suspPayloadLoadMassParams.m
@@ -0,0 +1,25 @@
+function susp_payload = suspPayloadLoadMassParams( filename, susp_payload )
+% SUSPPAYLOADLOADMASSPARAMS loads a suspended payloads body parameters
+% (.body, .config) and appends them to susp_payload
+
+% Disclamer:
+%   SPDX-License-Identifier: GPL-3.0-only
+% 
+%   Copyright (C) 2023 Jonas Withelm
+%   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
+% *************************************************************************
+
+run(filename)
+
+susp_payload.body   = susp_payload_total.body;
+susp_payload.config = susp_payload_total.config;
+
+%{
+susp_payload.rod.body   = rod.body;
+susp_payload.rod.config = rod.config;
+
+susp_payload.payload.body   = payload.body;
+susp_payload.payload.config = payload.config;
+%}
+
+end
