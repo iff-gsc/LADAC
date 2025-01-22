794794cd8d436804fa2ad13893abaf3f82f7161d
Yannic Beyer <y.beyer@tu-bs.de>
2025-01-22 19:40:07 +0100

    Autopilot LindiCopter: Remove obsolete parameters

--- control/autopilots/LindiCopter/lindiCopter_params_notune_default.m
+++ control/autopilots/LindiCopter/lindiCopter_params_notune_default.m
@@ -7,10 +7,3 @@
 
 % loop time, must be the same as 1/AP:SCHED_LOOP_RATE
 param.ts = 0.0025;
-
-% maximum number of waypoints
-param.traj.wpmax = 4;
-% boolean if last and first waypoint should be connected (true) or not
-param.traj.cycle = true;
-% degree of spline polynomials
-param.traj.degree = 5;
