3d9b71fd9041564f5d8891c66f386e468541411b
Yannic Beyer <y.beyer@tu-bs.de>
2025-07-15 20:40:02 +0200

    FlightGear: Move default location to BWE and delete obsolete param file

--- utilities/interfaces_external_programs/FlightGear_visualization/flightgearRefPos_params_default.m
+++ utilities/interfaces_external_programs/FlightGear_visualization/flightgearRefPos_params_default.m
@@ -1,8 +1,8 @@
-% ** parameters for reference position ** 
+%  ** Parameter file of FlightGear reference position (default) **
 
 % initial latitude, in deg
-param.lat = 37.6117;
+param.lat = 52.318701;
 % initial longitude, in deg
-param.lon = -122.37822;
+param.lon = 10.547244;
 % initial altitude, in m
-param.alt = 150;
+param.alt = 80.077; % 0.25m above ground level
