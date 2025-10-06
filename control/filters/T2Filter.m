1ee00a744fee1197d3a8841c23ef5ca3eb121e31
Yannic Beyer <y.beyer@tu-bs.de>
2025-10-06 17:26:05 +0200

    Filters: Improve T2 filter performance (for codegen)
    - The filter now support multidimensional inputs
    - The expm function was replaced by an
      analytic computation which is faster by a
      factor greater than 10 and which avoid
      code generation issues.
    - The inverse of the A matrix was also replaced
      by an analytic computation which is slightly
      faster.
    - The number of private properties was reduced
      to a minimum in order reduce the required
      memory

--- control/filters/T2Filter.m
+++ control/filters/T2Filter.m
@@ -1,7 +1,7 @@
 classdef T2Filter < matlab.System
     properties
         % Cutoff frequency (rad/s)
-        omega_0	= 2*pi*80;
+        omega_0	= 100;
         % Damping ratio
         d = 1;
         % Sample time (s)
@@ -9,9 +9,9 @@ classdef T2Filter < matlab.System
     end
     properties(Nontunable)
         % Enable velocity output
-        is_vel (1,1) logical = false;
+        is_vel = false;
         % Enable acceleration output
-        is_acc (1,1) logical = false;
+        is_acc = false;
     end
     properties(DiscreteState)
         x
@@ -19,53 +19,68 @@ classdef T2Filter < matlab.System
     properties(Access = private)
         Ad
         Bd
-        Cd
-        Dd
         T = 1/400;
-        last_omega_0;
-        last_d;
-        u0              = 0;
-        is_init         = true;
+        last_omega_0
+        last_d
+        is_init = true;
     end
     methods(Access = protected)
-        function setupImpl(obj)
+        function setupImpl(obj,u)
             setAd(obj);
             setBd(obj);
-            setCd(obj);
-            setDd(obj);
-            obj.x = zeros(2,1);
+            size_in = propagatedInputSize(obj,1);
+            n_i = prod(size_in);
+            obj.x = zeros(2,n_i,class(u));
             obj.last_omega_0 = obj.omega_0;
             obj.last_d = obj.d;
         end
 
         function varargout = stepImpl(obj,u)
             if obj.is_init
-                init(obj,u);
+                resetCustom(obj,u);
+                obj.is_init = false;
             end
             updateParams(obj);
             
-            y = obj.Cd*obj.x + obj.Dd*u;
-            obj.x = obj.Ad*obj.x + obj.Bd*u;
+            size_in = propagatedInputSize(obj,1);
+            n_i = prod(size_in);
+            n_o = getNumOutputsImpl(obj);
+            y = zeros(n_o,n_i,class(u));
             
-            if ~obj.is_vel && ~obj.is_acc
-                varargout{1} = y;
-            elseif (obj.is_vel && ~obj.is_acc) || (~obj.is_vel && obj.is_acc)
-                varargout{1} = y(1);
-                varargout{2} = y(2);
-            else
-                varargout{1} = y(1);
-                varargout{2} = y(2);
-                varargout{3} = y(3);
+            Cd = getCd(obj);
+            Dd = getDd(obj);
+            for i = 1:size(obj.x,2)
+                y(:,i) = Cd*obj.x(:,i) + Dd*u(i);
+                obj.x(:,i) = obj.Ad*obj.x(:,i) + obj.Bd*u(i);
+                if any(isnan(obj.x(:,i))) || any(isinf(obj.x(:,i)))
+                    resetCustom(obj,u);
+                end 
             end
             
-            obj.is_init = false;
-        end
-
-        function resetImpl(obj)
-            obj.x(:) = 0;
-            obj.is_init = true;
+            % reshape output to match input size
+            if ~obj.is_vel && ~obj.is_acc
+                varargout{1} = reshape(y,size_in);
+            elseif (obj.is_vel && ~obj.is_acc) || (~obj.is_vel && obj.is_acc)
+                varargout{1} = reshape(y(1,:),size_in);
+                varargout{2} = reshape(y(2,:),size_in);
+            else
+                varargout{1} = reshape(y(1,:),size_in);
+                varargout{2} = reshape(y(2,:),size_in);
+                varargout{3} = reshape(y(3,:),size_in);
+            end
+            
         end
         
+        % resetImpl does not allow input u
+        function resetCustom(obj,u)
+            obj.x(1,:) = u(:);
+            obj.x(2,:) = 0;
+        end
+        
+        function resetImpl(obj)
+            obj.x(:) = 0;
+        end
+
         function num = getNumOutputsImpl(obj)
             if ~obj.is_vel && ~obj.is_acc
                 num = 1;
@@ -92,53 +107,86 @@ classdef T2Filter < matlab.System
             if (obj.omega_0~=obj.last_omega_0 || obj.d~=obj.last_d)
                 setAd(obj);
                 setBd(obj);
-                setCd(obj);
-                setDd(obj);
                 obj.last_omega_0 = obj.omega_0;
                 obj.last_d = obj.d;
             end
         end
         
         function setAd(obj)
-            A = [0 1;
-                -obj.omega_0^2  -2*obj.d*obj.omega_0];
-            obj.Ad = expm(A*obj.T);
+            % the expm function is computationally expensive,
+            % so compute Ad = expm(A*T) analytically
+            omega0 = obj.omega_0;
+            D = obj.d;
+            % don't allow unstable system
+            if D < 0
+                D(:) = 0;
+            end
+            t = obj.T*ones(1,class(obj.omega_0));
+            alpha = D * omega0;
+            if D < 1
+                % underdamped
+                wd = omega0 * sqrt(max(0, 1 - D^2));
+                if abs(wd) < eps(D)
+                    wd(:) = eps(D);
+                end
+                expm_factor = exp(-alpha * t);
+                wt = wd * t;
+                sin_wt = sin(wt);
+                cos_wt = cos(wt);
+                a11 = cos_wt + alpha/wd * sin_wt;
+                a12 = 1/wd * sin_wt;
+                a21 = -omega0^2/wd * sin_wt;
+                a22 = cos_wt - alpha/wd * sin_wt;
+            elseif abs(D - 1) <= 1e-12
+                % critically damped
+                expm_factor = exp(-omega0 * t);
+                a11 = 1 + omega0 * t;
+                a12 = t;
+                a21 = -omega0^2 * t;
+                a22 = 1 - omega0 * t;
+            else
+                % overdamped
+                s = omega0 * sqrt(D^2 - 1);
+                expm_factor = exp(-alpha * t);
+                sh = sinh(s*t);
+                ch = cosh(s*t);
+                a11 = ch + alpha/s * sh;
+                a12 = 1/s * sh;
+                a21 = -omega0^2/s * sh;
+                a22 = ch - alpha/s * sh;
+            end
+            obj.Ad = expm_factor * [a11, a12; a21, a22];
         end
         
         function setBd(obj)
-            A = [0 1;
-                -obj.omega_0^2  -2*obj.d*obj.omega_0];
             B = [0; obj.omega_0^2];
-            obj.Bd = A \ (obj.Ad-eye(2)) * B;
+            A_inv = [ -2*obj.d/obj.omega_0, -1/obj.omega_0^2; 1, 0 ];
+            obj.Bd = A_inv * (obj.Ad-eye(2)) * B;
         end
         
-        function setCd(obj)
+        function Cd = getCd(obj)
             if ~obj.is_vel && ~obj.is_acc
-                obj.Cd = [1 0];
+                Cd = [1 0]*ones(1,class(obj.omega_0));
             elseif obj.is_vel && ~obj.is_acc
-                obj.Cd = [1 0; 0 1];
+                Cd = [1 0; 0 1]*ones(1,class(obj.omega_0));
             elseif ~obj.is_vel && obj.is_acc
-                obj.Cd = [1 0; 0 0];
+                Cd = [1 0; 0 0]*ones(1,class(obj.omega_0));
             else
-                obj.Cd = [1 0; 0 1; -obj.omega_0^2, -2*obj.d*obj.omega_0];
+                Cd = [1 0; 0 1; -obj.omega_0^2, -2*obj.d*obj.omega_0]...
+                    *ones(1,class(obj.omega_0));
             end
         end
         
-        function setDd(obj)
+        function Dd = getDd(obj)
             if ~obj.is_vel && ~obj.is_acc
-                obj.Dd = 0;
+                Dd = zeros(1,class(obj.omega_0));
             elseif obj.is_vel && ~obj.is_acc
-                obj.Dd = [0; 0];
+                Dd = zeros(2,1,class(obj.omega_0));
             elseif ~obj.is_vel && obj.is_acc
-                obj.Dd = [0; obj.omega_0^2];
+                Dd = [0; obj.omega_0^2]*ones(1,class(obj.omega_0));
             else
-                obj.Dd = [0; 0; obj.omega_0^2];
+                Dd = [0; 0; obj.omega_0^2]*ones(1,class(obj.omega_0));
             end
-        end
-        
-        function init(obj,u)
-            obj.x(1) = u;
-            obj.x(2) = 0;
         end
         
     end
